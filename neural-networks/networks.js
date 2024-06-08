54a4c02bb3a1e975ef9dc1af37dd6b0f5a5a2b97
Ebo Arcadia <yiboli.coding@gmail.com>
2024-06-08 15:05:20 -0400

    initial setup level class

---
+++ neural-networks/networks.js
@@ -0,0 +1,26 @@
+class Level {
+  constructor(inputCount, outputCount) {
+    this.inputs = new Array(inputCount);
+    this.outputs = new Array(outputCount);
+    this.biases = new Array(outputCount);
+
+    this.weights = [];
+    for (let i = 0; i < inputCount; i++) {
+      this.weights[i] = new Array(outputCount);
+    }
+
+    Level.#randomize(this);
+  }
+
+  static #randomize(level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1;
+      }
+    }
+
+    for (let i = 0; i < level.biases.length; j++) {
+      level.weights[i][j] = Math.random() * 2 - 1;
+    }
+  }
+}
