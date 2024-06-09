12fec71b72c4811e098ab3f4906fbc86117d31e6
Ebo Arcadia <yiboli.coding@gmail.com>
2024-06-09 12:31:06 -0400

    feedforward func

--- neural-networks/networks.js
+++ neural-networks/networks.js
@@ -23,4 +23,23 @@ class Level {
       level.weights[i][j] = Math.random() * 2 - 1;
     }
   }
+
+  static feedForward(givenInputs, level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      level.inputs[i] = givenInputs[i];
+    }
+    for (let i = 0; i < level.outputs.length; i++) {
+      let sum = 0;
+      for (let j = 0; j < level.inputs.length; j++) {
+        sum += level.inputs[j] * level.weights[j][i];
+      }
+
+      if (sum > level.biases[i]) {
+        level.outputs[i] = 1;
+      } else {
+        level.outputs[i] = 0;
+      }
+    }
+    return level.outputs;
+  }
 }
