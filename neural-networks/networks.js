6da5c29dc76f242792215b88058e612d12b5e926
Ebo Arcadia <yiboli.coding@gmail.com>
2024-06-10 13:37:50 -0400

    hidden layers

--- neural-networks/networks.js
+++ neural-networks/networks.js
@@ -1,3 +1,20 @@
+class NeuralNetwork {
+  constructor(neuronCounts) {
+    this.levels = [];
+    for (let i = 0; i < neuronCounts.length - 1; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+    }
+  }
+
+  static feedForward(givenInputs, network) {
+    let outputs = Level.feedForward(givenInputs, network.levels[0]);
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feedForward(outputs, network.level[i]);
+    }
+    return outputs;
+  }
+}
+
 class Level {
   constructor(inputCount, outputCount) {
     this.inputs = new Array(inputCount);
