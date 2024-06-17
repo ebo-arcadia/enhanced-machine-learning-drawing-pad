4f6b51961245de5d69ca4667b2bc053731bb934a
Ebo Arcadia <yiboli.coding@gmail.com>
2024-06-17 12:37:43 -0400

    connecting input and output

--- neural-networks/visualizer.js
+++ neural-networks/visualizer.js
@@ -14,13 +14,21 @@ class Visualizer {
     const bottom = top + height;
 
     const { inputs, outputs } = level;
+
+    for (let i = 0; i < inputs.length; i++) {
+      for (let j = 0; j < outputs.length; j++) {
+        ctx.beginPath();
+        ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom);
+        ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "orange";
+        ctx.stroke();
+      }
+    }
+
     const nodeRadius = 18;
     for (let i = 0; i < inputs.length; i++) {
-      const x = lerp(
-        left,
-        right,
-        inputs.length == 1 ? 0.5 : i / (inputs.length - 1)
-      );
+      const x = Visualizer.#getNodeX(inputs, i, left, right);
       ctx.beginPath();
       ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2);
       ctx.fillStyle = "white";
@@ -28,15 +36,19 @@ class Visualizer {
     }
 
     for (let i = 0; i < outputs.length; i++) {
-      const x = lerp(
-        left,
-        right,
-        inputs.length == 1 ? 0.5 : i / (outputs.length - 1)
-      );
+      const x = Visualizer.#getNodeX(outputs, i, left, right);
       ctx.beginPath();
       ctx.arc(x, top, nodeRadius, 0, Math.PI * 2);
       ctx.fillStyle = "white";
       ctx.fill();
     }
+  }
+
+  static #getNodeX(nodes, index, left, right) {
+    return lerp(
+      left,
+      right,
+      nodes.length == 1 ? 0.5 : index / (nodes.length - 1)
+    );
   }
 }
