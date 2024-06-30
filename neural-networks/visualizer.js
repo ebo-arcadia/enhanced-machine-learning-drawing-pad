126a26576e3007cb23b2f09a126e0a6a4423938f
Ebo Arcadia <yiboli.coding@gmail.com>
2024-06-30 15:58:28 -0400

    dynamically style weights neurons

--- neural-networks/visualizer.js
+++ neural-networks/visualizer.js
@@ -13,7 +13,7 @@ class Visualizer {
     const right = left + width;
     const bottom = top + height;
 
-    const { inputs, outputs } = level;
+    const { inputs, outputs, weights } = level;
 
     for (let i = 0; i < inputs.length; i++) {
       for (let j = 0; j < outputs.length; j++) {
@@ -21,7 +21,12 @@ class Visualizer {
         ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom);
         ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top);
         ctx.lineWidth = 2;
-        ctx.strokeStyle = "orange";
+        const value = weights[i][j];
+        const alpha = Math.abs(value);
+        const R = value < 0 ? 0 : 255;
+        const G = R;
+        const B = value > 0 ? 0 : 255;
+        ctx.strokeStyle = "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
         ctx.stroke();
       }
     }
