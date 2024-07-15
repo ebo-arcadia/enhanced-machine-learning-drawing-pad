ee279c115c50c520bf54d9020899cbd99867954d
Ebo Arcadia <yiboli.coding@gmail.com>
2024-07-15 11:16:31 -0400

    mlp with higher accuracy

--- python/mlp.py
+++ python/mlp.py
@@ -19,5 +19,33 @@ X, y = readFeatureFile("/Users/ebolee/repo/enhanced-machine-learning-drawing-pad
 accuracy = mlp.score(X, y)
 print("Accuracy:", accuracy)
 
-print(mlp.intercepts_)
-print(mlp.coefs_)
+classes = [
+    "car", "fish", "house", "tree", "bicycle", "guitar", "pencil", "clock"
+]
+
+jsonObj = {
+    "neuronCounts": [len(X[0]), hidden, len(classes)],
+    "classes": classes,
+    "network": {
+        "levels": []
+    }
+}
+
+for i in range(0, len(mlp.coefs_)):
+    level = {
+        "weights": mlp.coefs_[i].tolist(),
+        "biases": mlp.intercepts_[i].tolist(),
+        "inputs": [0]*len(mlp.coefs_[i]),
+        "outputs": [0]*len(mlp.intercepts_[i])
+    }
+    
+    jsonObj["network"]["levels"].append(level)
+
+
+import json
+json_object = json.dumps(jsonObj, indent=2)
+
+with open("/Users/ebolee/repo/enhanced-machine-learning-drawing-pad/data/models/model.json", "w") as outfile:
+    outfile.write(json_object)
+with open("/Users/ebolee/repo/enhanced-machine-learning-drawing-pad/common/js_objects/model.js", "w") as outfile:
+    outfile.write("const model = "+json_object+";")
